496e6ffe2aa8c522d817011148a5d199550f7b54
Codely456 <theb12969@gmail.com>
2025-03-20 20:53:56 +0530

    Update deployment configuration

--- next.config.js
+++ next.config.js
@@ -4,10 +4,7 @@ const nextConfig = {
   images: {
     domains: ['www.google.com', 'lh3.googleusercontent.com'], // Allow Google profile images
   },
-  // Add experimental features to handle file system permissions
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
-  },
+  outputFileTracingRoot: process.cwd(),
   // Disable file system trace in development
   webpack: (config, { dev }) => {
     if (dev) {
